7c298f42bc007e8b3c30a4eaa2fdb88baed95f12
lsavoie <twosavoie@gmail.com>
2018-09-11 01:08:37 -0500

    make a new person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,10 +3,41 @@ get '/people' do
   erb :"/people/index"
 end
 
+#get '/' do  #had in my code, not in answer code. still need?
+#  @people = Person.all
+#  erb :"/people/index"
+#end
+
+# must be before get'/:id' so a new person is done by default (reword)
+get '/people/new' do
+  @person = Person.new
+  erb :"/people/new"
+end
+
+#get '/new' do  #had in my code, not in answer code. still need?
+#  erb :"/people/new"
+#end
+
+post '/people' do
+  if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+  end
+
+  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+
+  redirect "/people/#{person.id}"
+end
+
 get '/people/:id' do
   @person = Person.find(params[:id])
   birth_path_num = Person.get_birth_path_num(@person.birthdate.strftime("%m%d%Y"))
   @message = Person.get_message(birth_path_num)
   erb :"/people/show"
-  #{}"This is a person show page!"
 end
+
+#get '/:id' do  #had in my code, not in answer code. still need?
+#  @person = Person.find(params[:id])
+#  erb :"people/show"
+#end
--- spec/people_spec.rb
+++ spec/people_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'Our Person Class' do
   include SpecHelper
-  
+
   it 'creates a Person' do
-    person = Person.create(first_name: "Sample", last_name: "Person", birthdate: (DateTime.now - 25.years))
+    person = Person.create(first_name: "Miss", last_name: "Piggy", birthdate: DateTime.now - 40.years )
     expect(person.present?).to be(true)
     person.delete
   end
