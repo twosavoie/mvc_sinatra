afaf1fe9c6dc7d700f83254a0f9d28457ec818a8
lsavoie <twosavoie@gmail.com>
2018-09-18 18:37:22 -0500

    back to the same

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -10,33 +10,19 @@ get '/people/new' do
 end
 
 post '/people' do
-# if input.match(/^[0-9]+[0-9]$/).nil?
-#    @error = "You should enter a valid birthdate in the form of mmddyyyy."
-#    erb :form
-#  end #Didn't work at all
-
 birthdate = params[:birthdate].tr("/-", "") #This allows ""-/ in /
 if Person.valid_birthdate(birthdate)
-  birth_path_num = Person.get_birth_path_num(birthdate)
+ birth_path_num = Person.get_birth_path_num(birthdate)
   redirect "/message/#{birth_path_num}"
 else
   @error = "You should enter a valid birthdate in the form of mmddyyyy."
-  erb :form
+  erb :form #don't think I need this
 end
-
-
 #  if params[:birthdate].include?("-")
-#  if params[:birthdate].include?("-/") doesn't work
 #    birthdate = params[:birthdate]
 #  else
 #    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
-#  else
-#    erb :"/people/new"  #made a new person with dashes in Safari. Then after I wrote this it didn't (11-11-1111 works still) but did give me the error.
 #  end
-
-
-#  @person = Person.new(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
-#this works as expected, getting errors in both Google and Safari. However, date must be input with slashes in safari to work. Doesn't work with dashes or 8 straight numers. This is no longer true - actually the opposite. I don't kow what changed :)
 
   @person = Person.new(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
 
